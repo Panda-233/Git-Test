eeb4684de225e3bde8818f814cafcac1085596ab
admin <2241943820@qq.com>
2022-07-08 18:26:29 +0800

    third commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -3,5 +3,6 @@ public class GitTest {
         System.out.println("Hello Git~");
         //恢复 HEAD 的方式就是用 git switch master  或者 git checkout master 就可以回最新版本啦
         System.out.println("Hello Git again~");
+        System.out.println("Hello Git again again~");
     }
 }
