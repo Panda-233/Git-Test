50a29ef46544e54717beefb234853f71b4b645bf
admin <2241943820@qq.com>
2022-07-08 18:25:07 +0800

    second commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -1,5 +1,7 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git~");
+        //恢复 HEAD 的方式就是用 git switch master  或者 git checkout master 就可以回最新版本啦
+        System.out.println("Hello Git again~");
     }
 }
