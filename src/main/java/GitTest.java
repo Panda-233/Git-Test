447a561b6101ab6b5a35981717d427d5d691c797
admin <2241943820@qq.com>
2022-07-08 18:38:14 +0800

    hot-fix commit ---> 1

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -1,9 +1,10 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git~");
-        //恢复 HEAD 的方式就是用 git switch master  或者 git checkout master 就可以回最新版本啦
         System.out.println("Hello Git again~");
         System.out.println("Hello Git again again~");
         System.out.println("Hello Git again again again~");
+        System.out.println("Hello Git again again again~");
+        System.out.println("Hello Git again again again again~");
     }
 }
